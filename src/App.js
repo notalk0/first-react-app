807474d72f60d19cb5f0f3463427a7f7489c0a37
Reina Liew <notalk_0@hotmail.com>
2019-05-27 20:35:57 +0800

    Finish

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Tweet from './tweet';
+
+function App() {
+  const [users, setUsers] = useState([
+    { name: 'Ed', message: 'Hello there' },
+    { name: 'John', message: 'I am John Snow' },
+    { name: 'Traversy', message: 'I am awesome' }
+  ]);
+
+  return (
+    <div className="app">
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
